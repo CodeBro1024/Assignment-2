74127f8d4b8844d2d10e134c02c9bbb9f3cbb6d0
CodeBro1024 <swapnanillmondal@gmail.com>
2026-02-21 01:27:15 +0530

    Delete Task2.py

--- Task2.py
+++
@@ -1,6 +0,0 @@
-first_name = str(input("Enter your first name: "))
-last_name = str(input("Enter your last name: "))
-
-full_name = first_name +" "+ last_name 
-
-print("Hello,",full_name,"Long time no see!")
